385c68df7a24827ff97d58d43e3efb1b1988a7f3
NatashaMikhaltsova <natashamikhaltsova@gmail.com>
2021-03-10 23:21:28 +0300

    feat: implement toReadable function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    let res = "";
+    const simpleNumbers = [
+        "",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    const tenthsNumbers = [
+        "",
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    number = ("000" + number).substr(-3).match(/^(\d{1})(\d{2})$/);
+    res += number[0] == "000" ? "zero" : "";
+    res +=
+        number[1] != 0
+            ? (simpleNumbers[Number(number[1])] + " " ||
+                  tenthsNumbers[number[1][0]] +
+                      " " +
+                      simpleNumbers[number[1][1]]) + "hundred "
+            : "";
+    res +=
+        number[2] != 0
+            ? simpleNumbers[Number(number[2])] ||
+              tenthsNumbers[number[2][0]] + " " + simpleNumbers[number[2][1]]
+            : "";
+    return res.trim();
+};
